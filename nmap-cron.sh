9e0671a5a80f61d8020b2323c1d865f1bcc9620f
Takashi SASAKI <takashi316@gmail.com>
2020-03-02 09:43:29 +0000

    nmap-cron.sh uses mylocalnet

--- nmap-cron.sh
+++ nmap-cron.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-nmap_cron_target=133.71.3.0/24
+nmap_cron_target=`mylocalnet`
 nmap_cron_dir=/mnt/nmap
 nmap_cron_filename=`date -Iseconds`.xml
 nmap_cron_path=${nmap_cron_dir}/${nmap_cron_filename}
 echo ${namp_cron_path}
 if [ -n "$PS1" ]; then
-	nmap -v -oX ${nmap_cron_path} ${nmap_cron_target}
+	nmap -v --max-retries 1 --max-rate 1 -oX ${nmap_cron_path} ${nmap_cron_target}
 else
-	nmap --max-rate=1 -oX ${nmap_cron_path} ${nmap_cron_target}
+	nmap    --max-retries 1 --max-rate 1 -oX ${nmap_cron_path} ${nmap_cron_target}
 fi
 
